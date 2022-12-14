f383a404466ba8ead4c306b3effec3107471419e
22584943 <22584943@stu.mmu.ac.uk>
2022-12-14 17:26:15 +0000

    added util, html dump, getHeader module

--- src/getHeader.java
+++ src/getHeader.java
@@ -2,60 +2,99 @@ public class getHeader {
     static public String get() {
         return "<head>" +
                 "<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css\"/>" +
-//                "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">" +
+                "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">" +
+                "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>" +
                 "        <style>" +
-                "            * {font-family: Futura, \"Trebuchet MS\", Arial, sans-serif;" +
-                "                padding: 0;" +
-                "                margin: 0;" +
-                "                text-decoration: none;" +
-                " box-sizing: border-box;" +
-                "            }" +
-                "            a {" +
-                "                color: inherit;" +
-                "            }" +
-                "            header h1 {" +
-                "                color: white;" +
-                "            }" +
-                " .table-cont {" +
+                " * {\n" +
+                "    font-family: Futura, \"Trebuchet MS\", Arial, sans-serif;\n" +
+                "    padding: 0;\n" +
+                "    margin: 0;\n" +
+                "    text-decoration: none!important;\n" +
+                "    box-sizing: border-box!important;\n" +
+                "}\n" +
+                "a {\n" +
+                "   color: inherit!important;\n" +
+                "}" +
+                "header h1 {\n" +
+                "   color: white;\n" +
+                "   margin: 0!important;" +
+                "}\n" +
+                "\n" +
+                ".table-cont, .form-wrapper, .flex-center {\n" +
+                "    width: 100%;\n" +
+                "    height: 100%;\n" +
+                "    display: flex;\n" +
+                "    justify-content:center;\n" +
+                "    align-items: center;\n" +
+                "}\n" +
+                ".flex-down {\n" +
+                "    flex-direction: column;\n" +
+                "}" +
+                "header {\n" +
+                "   background: #000;\n" +
+                "   display: flex;\n" +
+                "   padding: 0.75em 3%;\n" +
+                "   justify-content: space-between;\n" +
+                "   align-items: center;\n" +
+                "   color: white;\n" +
+                "}\n" +
+                "header i {\n" +
+                "   padding: 0.15em 0.5em;\n" +
+                "   font-size: 1.5em;\n" +
+                "\n" +
+                "}\n" +
+                "header h1 {\n" +
+                "   line-height: 1.5em;\n" +
+                "}\n" +
+                ".logo-cont, i, nav > ul {\n" +
+                "   display: flex;\n" +
+                "   justify-content: center;\n" +
+                "   align-items: center;\n" +
+                "}\n" +
+                "nav ul {\n" +
+                "   list-style-type: none;\n" +
+                "   padding: 0;\n" +
+                "   font-size: 1.25em;\n" +
+                "\n" +
+                "}\n" +
+                "\n" +
+                "nav ul li {\n" +
+                "   padding: 0.5em;\n" +
+                "}\n" +
+                "nav ul li:hover {\n" +
+                "   cursor: pointer;\n" +
+                "}" +
+                ".form-wrapper {\n" +
+                "    flex-direction: column;\n" +
+                "    justify-content: flex-start;" +
+                "    padding-top: 200px;" +
+                "}" +
+                "form {\n" +
+                "    display: flex;\n" +
+                "    flex-direction: column;\n" +
+                "    justify-content: center;\n" +
+                "    align-items: center;\n" +
+                "    min-width: 250px; \n" +
+                "    padding-top: 2em;" +
+                "}\n" +
+                "form button[type=\"submit\"] {\n" +
+                "    width: 100%;\n" +
+                "    background: navy;\n" +
+                "    color: white;\n" +
+                "    text-transform: uppercase;\n" +
+                "    border: none;"+
+                "}" +
+                "form input, button[type=\"submit\"] {\n" +
                 "   width: 100%;" +
-                "height: 100%;" +
-                "display: flex;" +
-                "justify-content:center;" +
-                "align-items: center;}" +
-                "            header {" +
-                "                background: #000;" +
-                "                display: flex;" +
-                "                padding: 0.75em 3%;" +
-                "                justify-content: space-between;" +
-                "                align-items: center;" +
-                "                color: white;" +
-                "            }" +
-                "            header i {" +
-                "                padding: 0.15em 0.5em;" +
-                "                font-size: 1.5em;" +
-                "" +
-                "            }" +
-                "            header h1 {" +
-                "                line-height: 1.5em;" +
-                "            }" +
-                "            .logo-cont, i, nav > ul {" +
-                "                display: flex;" +
-                "                justify-content: center;" +
-                "                align-items: center;" +
-                "            }" +
-                "            nav ul {" +
-                "                list-style-type: none;" +
-                "                padding: 0;" +
-                "                font-size: 1.25em;" +
-
-                "            }" +
-
-                "            nav ul li {" +
-                "                padding: 0.5em;" +
-                "            }" +
-                "            nav ul li:hover {" +
-                "                cursor: pointer;" +
-                "            }" +
+                "    padding: 0.5em;\n" +
+                "    margin-bottom: 1em;\n" +
+                "}" +
+                "form .form-row {\n" +
+                "    width: 100%;" +
+                "    display: flex;\n" +
+                "    flex-direction: column;\n" +
+                "    align-items: flex-start;\n" +
+                "}"+
                 "        </style>" +
                 "    </head>" +
                 "    <body>" +
@@ -68,7 +107,7 @@ public class getHeader {
                 "                <ul>" +
                 "                    <li><a href=\"/products\">Products</a></li>" +
                 "                    <li><a href=\"/customers\">Customers</a></li>" +
-                "                    <li>Login</li>" +
+                "                    <li><a href=\"/login\">Login</a></li>" +
                 "                </ul>" +
                 "            </nav>" +
                 "        </header>";
