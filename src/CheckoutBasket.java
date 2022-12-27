dd2d5c7545ce8246b32f26d61e42e053904ea064
Adam White <adamfrankwhite@gmail.com>
2022-12-27 20:16:17 +0000

    access and output responseObject

--- src/CheckoutBasket.java
+++ src/CheckoutBasket.java
@@ -17,7 +17,7 @@ public class CheckoutBasket implements HttpHandler{
         BufferedWriter out = new BufferedWriter(
                 new OutputStreamWriter(he.getResponseBody()));
 
-
+        ProductDAO productDAO = new ProductDAO();
         BasketDAO basketDAO = new BasketDAO();
         ArrayList<Product> basket = null;
         try {
@@ -27,16 +27,29 @@ public class CheckoutBasket implements HttpHandler{
         }
 
         try {
-            // empty basket
-            boolean checkoutBasketSuccess = basketDAO.checkoutBasket(basket);
-            String outputMessage = checkoutBasketSuccess ? "Checkout successful" : "Error: Please try again";
+            // fetch response object with message. empty basket
+            ResponseObject checkoutBasketResponse = basketDAO.checkoutBasket(basket);
+            StringBuilder outputMessage = new StringBuilder(checkoutBasketResponse.success ? "Checkout successful" : "Error: Please try again");
+            if (checkoutBasketResponse.errorMessage.length() > 0) {
+                String[] errorMsg = checkoutBasketResponse.errorMessage.split(";");
+                ArrayList<String> productsOutOfStock = new ArrayList<String>();
 
+                for (String error : errorMsg) {
+                    String id = error.split(", ")[0];
+                    productsOutOfStock.add(id);
+                }
+
+                for (String productID : productsOutOfStock) {
+                    outputMessage.append("<li>").append(productDAO.getProductByID(Integer.parseInt(productID)).getName()).append("out of Stock</li>");
+                }
+
+            }
             out.write(
                     "<html>" +
                             getHeader.get() +
                             "<body>" +
                             "<div class=\"flex-center flex-down\">" +
-                            "<h1>" + outputMessage + "</h1>" +
+                            "<ul>" + outputMessage + "</ul>" +
 
                             "</div>" +
                             "</body>" +
