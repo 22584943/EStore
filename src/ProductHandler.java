1d09406a0946e619dad5da3ecae497e626120eee
Adam White <adamfrankwhite@gmail.com>
2023-01-07 21:29:52 +0000

    added script and convert java coll into json

--- src/ProductHandler.java
+++ src/ProductHandler.java
@@ -14,6 +14,7 @@ public class ProductHandler implements HttpHandler{
         BufferedWriter out = new BufferedWriter(
                 new OutputStreamWriter(he.getResponseBody() ));
         ProductDAO products = new ProductDAO();
+        String collJSON = "";
         ArrayList<Product> coll;
 
 
@@ -23,9 +24,34 @@ public class ProductHandler implements HttpHandler{
         boolean isLoggedIn = ud.isLoggedIn();
         // Condition button render
         String AddDeleteColumn = isLoggedIn ? "<div class=\"cell-cont\"></div><div class=\"cell-cont\"></div>" : "";
+
+        // Autocomplete script
+
+
         try {
             String showAdd = isLoggedIn ? "<div><a href=\"/products/add\"><span>Add Product</span></a></div>" : "";
             coll = products.getProducts();
+            collJSON = products.getProductsJSON();
+            System.out.println(collJSON);
+            String autoCompleteScript = "const searchInput = document.getElementById(\"searchInput\");\n" +
+                    "\n" +
+                    "const dropdownCont = document.getElementsByClassName(\"dropdown-cont\")[0]\n" +
+                    "const dropdownResults = document.getElementsByClassName(\"dropdown-results\")[0]\n" +
+                    "\n" +
+                    "let searchResults = '" + collJSON + "'" + "\n"+
+                    "searchResults = searchResults.substring(0, searchResults.length - 2)" + "\n" +
+                    "searchResults = \"[\" + searchResults + \"]\"" + "\n" +
+                    "console.log(JSON.parse(searchResults))\n" +
+//                    "searchResults = searchResults.replace('[','');"+
+//                    "searchResults = searchResults.replace(']','');"+
+//                    "console.log(searchResults)\n" +
+//                    "searchResults = searchResults.split(\"}, \")\n" +
+//                    "console.log(searchResults)\n" +
+                    "\n" +
+                    "searchInput.addEventListener(\"input\", (e)=> {\n" +
+                    "    console.log(e.target.value)\n" +
+                    "})";
+//            "let searchResults = \"" + coll + "\"\n" +
             ArrayList<String> categories = products.getCategories();
             out.write(
                     getHeader.get() +
@@ -39,7 +65,7 @@ public class ProductHandler implements HttpHandler{
                             "                <option value=\"category\">category</option>" +
                             "                <option value=\"description\">description</option>\n" +
                             "            </select>\n" +
-                            "            <input name=\"query\" type=\"text\" />\n" +
+                            "            <div class=\"dropdown-cont\"><input id=\"searchInput\" name=\"query\" type=\"text\" /><ul id=\"dropdown-results\"></ul></div>\n" +
                             "           <button type=\"submit\">Search</button>" +
                             "        </form>" +
                             "       <div class=\"category-btn-cont\"><span>Categories: </span>"
@@ -75,7 +101,6 @@ public class ProductHandler implements HttpHandler{
             for (Product p: coll) {
                 DecimalFormat formatter = new DecimalFormat("0.00");
 
-                System.out.println(p.getName() + ", " + p.getPrice());
                 String formattedPrice = formatter.format(p.getPrice());
                 // Keep add/delete in conditional cell-col render, to maintain table styling
                 String showEditOrAddBtn = isLoggedIn ? "<span class=\"cell-col edit-btn-cell\"><a class=\"flex\" href=\"/products/edit?id=" + p.getID() +"\"><i class=\"bi bi-pencil-square\"></i><span>Edit</span></a><a class=\"flex\" href=\"/products/delete?id=" + p.getID() +"\"/><i class=\"bi bi-trash-fill\"></i></i><span>Delete</span></a>" : "<span class=\"product-basket-cell cell-cont\"><form action=\"/basket/add\" method=\"get\"><input type=\"hidden\" name=\"id\" value=\""+p.getID() + "\"/><input type=\"hidden\" name=\"currentStock\" value=\"" +p.getStock() + "\"/><input type=\"number\" value=\"1\" name=\"quantity\"/><button type=\"submit\">Add</button></form></span>";
@@ -102,6 +127,9 @@ public class ProductHandler implements HttpHandler{
                         "</div>" +
                             "</div>" +
                             showAdd +
+                                "<script>" +
+                                autoCompleteScript +
+                                "</script>"+
                             "</body>" +
                             "</html>"
             );
