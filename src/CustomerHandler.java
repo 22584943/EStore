0d1919f0b01d6f9d2f1a1042e18706857f790222
Adam White <adamfrankwhite@gmail.com>
2023-01-08 19:44:35 +0000

    changed isLoggedIn to isAdminLoggedIn, refactored

--- src/CustomerHandler.java
+++ src/CustomerHandler.java
@@ -16,60 +16,68 @@ public class CustomerHandler implements HttpHandler{
         CustomerDAO customers = new CustomerDAO();
         ArrayList<Customer> coll = new ArrayList<Customer>();
         UserDAO ud = new UserDAO();
-        // Check if logged in
-        boolean isLoggedIn = ud.isLoggedIn();
+        // Check if admin logged in
+        boolean isAdminLoggedIn = ud.isLoggedIn("Admin");
 
-        try {
-            coll = customers.getAllCustomers();
-            out.write(
-                    getHeader.get() +
-                            "<div class=\"table-cont\">" +
-                            "<table class=\"table table-dark table-striped\">" +
-                            "<thead>" +
-                                "<tr>" +
-                                    "<th class=\"col\">ID</th>" +
-                                    "<th class=\"col\">Name</th>" +
-                                    "<th class=\"col\">Address</th>" +
-                                    "<th class=\"col\">Email</th>" +
-                                    "<th class=\"col\">Telephone</th>" +
-                                "</tr>" +
-                            "</thead>" +
-                            "<tbody>"
-
-            );
-            String showAdd = isLoggedIn ? "<div><a href=\"/customers/add\"><span>Add Customer</span></a></div>" : "";
-            for (Customer c: coll) {
-                Address cAddress = c.getAddress();
-                String cAddressToString = cAddress.toString();
-                // Condition button render
-                String showEdit = isLoggedIn ? "<td><a href=\"/customers/edit?id=" + c.getID() + "\">Edit</a></td>" : "";
-                String showDelete = isLoggedIn ? "<td><a href=\"/customers/delete?id=" + c.getID() +"\">Delete</a></td>" : "";
+        // Authorised Access
+        if (isAdminLoggedIn) {
+            try {
+                coll = customers.getAllCustomers();
                 out.write(
-                        "<tr>" +
-                                "<td>" + c.getID() +"</td>" +
-                                "<td>" + c.getName() +"</td>" +
-                                "<td>" + c.getAddress() +"</td>" +
-                                "<td>" + c.getEmail() +"</td>" +
-                                "<td>" + c.getTelephone() +"</td>" +
-                                showEdit +
-                                showDelete +
-                            "</tr>"
+                        getHeader.get() +
+                                "<div class=\"table-cont\">" +
+                                "<table class=\"table table-dark table-striped\">" +
+                                "<thead>" +
+                                "<tr>" +
+                                "<th class=\"col\">ID</th>" +
+                                "<th class=\"col\">Name</th>" +
+                                "<th class=\"col\">Address</th>" +
+                                "<th class=\"col\">Email</th>" +
+                                "<th class=\"col\">Telephone</th>" +
+                                "</tr>" +
+                                "</thead>" +
+                                "<tbody>"
 
                 );
+
+                for (Customer c : coll) {
+                    Address cAddress = c.getAddress();
+                    String cAddressToString = cAddress.toString();
+                    out.write(
+                            "<tr>" +
+                                    "<td>" + c.getID() + "</td>" +
+                                    "<td>" + c.getName() + "</td>" +
+                                    "<td>" + c.getAddress() + "</td>" +
+                                    "<td>" + c.getEmail() + "</td>" +
+                                    "<td>" + c.getTelephone() + "</td>" +
+                                    "<td><a href=\"/customers/edit?id=" + c.getID() + "\">Edit</a></td>" +
+                                    "<td><a href=\"/customers/delete?id=" + c.getID() + "\">Delete</a></td>" +
+                                    "</tr>"
+
+                    );
+                }
+                out.write(
+
+
+                        "</tbody>" +
+                                "</table>" +
+                                "</div>" +
+                                "<div><a href=\"/customers/add\"><span>Add Customer</span></a></div>" +
+                                "</body>" +
+                                "</html>"
+                );
+                out.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
+        } else {
             out.write(
-
-
-                    "</tbody>" +
-                    "</table>" +
-                            "</div>" +
-                            showAdd +
-                "</body>" +
-            "</html>"
+                    getHeader.get() +
+                            "<h1>Access Denied</h1>" +
+                            "</body>" +
+                            "</html>"
             );
             out.close();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
 
 
