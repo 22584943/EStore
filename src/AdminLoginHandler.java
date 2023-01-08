8a256617d9b64aab1504b19322ff3aba91480a3a
Adam White <adamfrankwhite@gmail.com>
2023-01-08 19:46:23 +0000

    added AdminLoginHandler

---
+++ src/AdminLoginHandler.java
@@ -0,0 +1,81 @@
+import java.io.*;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class AdminLoginHandler implements HttpHandler{
+    public void handle(HttpExchange he) throws IOException {
+        he.sendResponseHeaders(200,0);
+        BufferedWriter out = new BufferedWriter(
+                new OutputStreamWriter(he.getResponseBody() ));
+
+
+
+        out.write(
+                getHeader.get() +
+
+
+                        "<div class=\"login-wrapper\">" +
+                        "<h2>Admin</h2>" +
+                        "<ul class=\"nav nav-tabs\" id=\"myTab\" role=\"tablist\">\n" +
+                        "            <li class=\"nav-item\" role=\"presentation\">\n" +
+                        "                <button class=\"nav-link active\" id=\"login-tab\" data-bs-toggle=\"tab\" data-bs-target=\"#login\" type=\"button\" role=\"tab\" aria-controls=\"login\" aria-selected=\"true\">Login</button>\n" +
+                        "            </li>\n" +
+                        "            <li class=\"nav-item\" role=\"presentation\">\n" +
+                        "                <button class=\"nav-link\" id=\"register-tab\" data-bs-toggle=\"tab\" data-bs-target=\"#register\" type=\"button\" role=\"tab\" aria-controls=\"register\" aria-selected=\"true\">Register</button>\n" +
+                        "            </li>\n" +
+                        "\n" +
+                        "        </ul>\n" +
+                        "        <div class=\"tab-content\" id=\"myTabContent\">\n" +
+                        "            <div class=\"tab-pane fade show active\" id=\"login\" role=\"tabpanel\" aria-labelledby=\"login-tab\">\n" +
+                        "                <form action=\"/admin-login-handler\" method=\"post\">\n" +
+                        "                    <div class=\"form-row\">\n" +
+                        "                        <label for=\"login-username\">Username</label>\n" +
+                        "                        <input id=\"login-username\" name=\"username\" type=\"text\" />\n" +
+                        "                    </div>\n" +
+                        "                    <div class=\"form-row\">\n" +
+                        "                        <label for=\"login-password\">Password</label>\n" +
+                        "                        <input id=\"login-password\" name=\"password\" type=\"password\" />\n" +
+                        "                    </div>\n" +
+                        "                    <div class=\"form-row\">\n" +
+                        "                        <button type=\"submit\">Login</button>\n" +
+                        "                    </div>\n" +
+                        "                </form>\n" +
+                        "            </div>\n" +
+                        "            <div class=\"tab-pane fade\" id=\"register\" role=\"register\" aria-labelledby=\"register-tab\">\n" +
+                        "                    <form action=\"/admin-register-handler\" method=\"post\">\n" +
+                        "                        <div class=\"form-row\">\n" +
+                        "                            <label for=\"reg-username\">Username</label>\n" +
+                        "                            <input id=\"reg-username\" name=\"username\"type=\"text\" />\n" +
+                        "                        </div>\n" +
+                        "                        <div class=\"form-row\">\n" +
+                        "                            <label for=\"reg-password\">Password</label>\n" +
+                        "                            <input id=\"reg-password\" name=\"password\"type=\"password\" />\n" +
+                        "                        </div>\n" +
+                        "<div class=\"form-row\">" +
+                        "  <label for=\"reg-confirm-password\">Confirm Password</label>" +
+                        "  <input id=\"reg-confirm-password\" type=\"password\" />" +
+                        "</div>"+
+                        "                        <div class=\"form-row\">\n" +
+                        "                            <button type=\"submit\">Register</button>\n" +
+                        "                        </div>\n" +
+                        "                    </form>\n" +
+                        "            </div>\n" +
+                        "\n" +
+                        "        </div>"+
+                        "</div>" +
+                        "    </body>" +
+                        "</html>"
+        );
+        out.close();
+
+
+
+
+    }
+
+}
